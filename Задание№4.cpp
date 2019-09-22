5600e6b32555418de8f7ef5f7b8da4fc06ab0c23
Sergey-Chushev <55636712+Sergey-Chushev@users.noreply.github.com>
2019-09-22 21:35:19 +0300

    Add files via upload

---
+++ Задание№4.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	{
+		int a, b, c;
+		setlocale(LC_ALL, "Russian");
+		cout << "с помощью дополнительной переменной\n";
+		cout << "a = ";
+		cin >> a;
+		cout << "b = ";
+		cin >> b;
+		c = a;
+		a = b;
+		b = c;
+		cout << " a = " << a << "\n";
+		cout << " b = " << b << "\n";
+		system("pause");
+	}
+	{
+		int a, b;
+        cout << "без помощи дополнительной переменной\n";
+		cout << " a = ";
+		cin >> a;
+		cout << " b = ";
+		cin >> b;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		
+
+		cout << " a = " << a << "\n";
+		cout << " b = " << b << "\n";
+	}
+	return 0;
+
+
+    
+}
+
