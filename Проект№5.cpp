00078c261d28f6d6fba5545fcfed567a8c6c25b5
Sergey-Chushev <55636712+Sergey-Chushev@users.noreply.github.com>
2019-09-22 21:33:42 +0300

    Add files via upload

---
+++ Проект№5.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+	double X, x, v, t, a;
+	cout << "x = ";
+	cin >>  x;
+	cout << "v = ";
+	cin >> v;
+	cout << "t = ";
+	cin >> t;
+	a = -9.8;
+	cout << "X = " << x + v * t + a * t * t / 2;
+	
+	
+
+	return 0;
+
+ 
+}
+
