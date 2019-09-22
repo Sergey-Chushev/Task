92cca6bb11ec6aa4187512392b796f2ede70e8d6
Sergey-Chushev <55636712+Sergey-Chushev@users.noreply.github.com>
2019-09-22 21:29:09 +0300

    Add files via upload

---
+++ Задание №2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	x = 3;
+	y = 3.14;
+	cout << " x = " << x <<"\n";
+	cout << " y = " << y << "\n";
+     double a, s;
+	a = 3;
+	s = 3.14;
+	cout << " a = " << a << "\n";
+	cout << " s = " << s << "\n";
+	return 0;
+}
