45c0755217018c927afc51d61c8979d0d8caf6d0
Sergey-Chushev <55636712+Sergey-Chushev@users.noreply.github.com>
2019-09-22 21:31:47 +0300

    Add files via upload

---
+++ Задание№3.cpp
@@ -0,0 +1,78 @@
+﻿
+#include <iostream>
+#include<Windows.h>
+using namespace std;
+int main()
+{
+	{
+		int x, y;
+		cout << "     int.\n";
+		cout << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+
+		cout << "x+y = " << x + y << "\n";
+		cout << "x-y = " << x - y << "\n";
+		cout << "x*y = " << x * y << "\n";
+		cout << "x/y = " << x / y << "\n";
+		system("pause");
+	}
+
+
+	{
+		double x, y;
+		cout << "     double.\n";
+		cout << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+		cout << "x+y = " << x + y << "\n";
+		cout << "x-y = " << x - y << "\n";
+		cout << "x*y = " << x * y << "\n";
+		cout << "x/y = " << x / y << "\n";
+		system("pause");
+	}
+
+	{
+		int x;
+		double y;
+		cout << " int(x) and double(y).\n";
+		cout << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+		cout << "x+y = " << x + y << "\n";
+		cout << "x-y = " << x - y << "\n";
+		cout << "x*y = " << x * y << "\n";
+		cout << "x/y = " << x / y << "\n";
+		system("pause");
+	}
+
+	{
+		double x;
+		int y;
+		cout << " double(x) and int(y).\n";
+		cout << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+		cout << "x+y = " << x + y << "\n";
+		cout << "x-y = " << x - y << "\n";
+		cout << "x*y = " << x * y << "\n";
+		cout << "x/y = " << x / y << "\n";
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
