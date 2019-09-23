ba9b946db834813a238cf57af110051d2373a035
Sergey-Chushev <55636712+Sergey-Chushev@users.noreply.github.com>
2019-09-23 19:37:12 +0300

    Add files via upload

---
+++ ЗАДАНИЕ №8.cpp
@@ -0,0 +1,50 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int M;
+	double x, y;
+	cout << " Введите первое число\n";
+	cin >> x;
+	cout << " Введите второе число\n";
+	cin >> y;
+	cout << " Выберите операцию: \n";
+	cout << " Сложение '1' \n";
+	cout << " Вычитание '2'\n";
+	cout << " Умножение '3'\n";
+	cout << " Деление '4'\n";
+	cin >> M;
+	switch (M)
+	{
+	case 1:
+
+	{
+		cout << x << "+" << y << "=" << x + y << endl;
+		break;
+	}
+	case 2:
+
+	{
+		cout << x << "-" << y << "=" << x - y << endl;
+		break;
+	}
+	case 3:
+	{
+		cout << x << "*" << y << "=" << x * y << endl;
+		break;
+	}
+	case 4:
+	{
+		cout << x << "/" << y << "=" << x / y << endl;
+		break;
+	}
+	default:
+		cout << "Ошибка" << endl;
+	}
+
+
+
+
+	}
